db1ef5bfc51ff15526abf5da9955fd24c6c9ce52
Dinod Tharinda <94352564+dinodtharinda@users.noreply.github.com>
2024-10-07 19:41:28 +0530

    clear project

--- lib/features/weather/domain/repositories/weather_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:flutter_tdd_testing/core/error/failure.dart';
-import 'package:flutter_tdd_testing/features/weather/domain/entities/weather.dart';
-
-abstract class WeatherRepository {
-  
-  Future<Either<Failure,WeatherEntity>> getCurrentWeather(String cityName);
-}
